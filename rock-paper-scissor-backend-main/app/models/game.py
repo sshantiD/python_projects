d17d57bbc0c58a7643e11319eb68e800830f28df
manju <manju@manjus-air.mynetworksettings.com>
2024-08-25 12:46:52 -0400

    deleted rock paper scissor main

--- rock-paper-scissor-backend-main/app/models/game.py
+++
@@ -1,8 +0,0 @@
-from app import db
-
-class Game(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(100), nullable = False)
-
-    def __init__(self, name):
-        self.name = name
--- rock-paper-scissor-backend-main/app/models/player.py
+++
@@ -1,8 +0,0 @@
-from app import db
-
-class Player(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(100), nullable = False)
-
-    def __init__(self, name):
-        self.name = name
