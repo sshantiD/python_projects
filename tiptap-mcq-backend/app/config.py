67a40fcd943cd962a61698cfc9b9522a581e05da
manju <manju@manjus-air.mynetworksettings.com>
2024-08-25 12:48:10 -0400

    unstaged

--- tiptap-mcq-backend/app/config.py
+++
@@ -1,9 +0,0 @@
-class DevelopmentConfig:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///../db/app.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-class TestingConfig:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///../db/test_app.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    
-MCQ_DATA_DUMP_FILE = "mcq_data.json"
